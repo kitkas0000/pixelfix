9a3df873c15ae637040b831e72840dadff9a6f7b
kitkas0000 <ng.dinhduc.2311@gmail.com>
2024-04-30 14:41:55 +0700

    Add Radix UI dependencies and update Clerk integration in package.json, page.tsx, and layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
+import Mobilebar from "@/components/shared/Mobilebar";
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+      <Mobilebar />
+
       <div className="root-containter">
         <div className="wrapper">{children}</div>
       </div>
